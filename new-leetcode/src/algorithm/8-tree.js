2f6817bd5bd47e6fa7fdbf54b61bc376d606d719
赵安东 <zhaoandong4213@sina.com>
2021-09-10 17:21:43 +0800

    tree end almost

--- new-leetcode/src/algorithm/8-tree.js
+++ new-leetcode/src/algorithm/8-tree.js
@@ -27,14 +27,31 @@ const BinarySearchTree = (function () {
     }
 
     static inOrderTraverseNode(node, callback) {
-      console.log(node);
+      // 3 5 6 7 8 9 10 11 12 13 14 15 18 20 25
+      // console.log(node);
       if (node !== null) {
         this.inOrderTraverseNode(node.left, callback);
         callback(node.key);
         this.inOrderTraverseNode(node.right, callback);
       }
     }
-    // 3 5 6 7 8 9 10 11 12 13 14 15 18 20 25
+
+    static preOrderTraverseNode(node, callback) {
+      if (node !== null) {
+        callback(node.key);
+        this.preOrderTraverseNode(node.left, callback);
+        this.preOrderTraverseNode(node.right, callback);
+      }
+    }
+
+    static postOrderTraverseNode(node, callback) {
+      if (node !== null) {
+        this.postOrderTraverseNode(node.left, callback);
+        this.postOrderTraverseNode(node.right, callback);
+        callback(node.key);
+      }
+    }
+
     // 向树中插入一个新的键
     insert(key) {
       let newNode = new Node(key);
@@ -49,6 +66,75 @@ const BinarySearchTree = (function () {
     // 通过中序遍历方式遍历所有节点
     inOrderTraverse(callback) {
       BinarySearchTree.inOrderTraverseNode(this.root, callback);
+    }
+
+    // 通过先序遍历方式遍历所有节点
+    preOrderTraverse(callback) {
+      BinarySearchTree.preOrderTraverseNode(this.root, callback);
+    }
+
+    // 通过后序遍历方式遍历所有节点
+    postOrderTraverse(callback) {
+      BinarySearchTree.postOrderTraverseNode(this.root, callback);
+    }
+
+    // minNode
+    static minNode(node) {
+      if (node) {
+        while (node && node.left !== null) {
+          node = node.left;
+        }
+        return node.key;
+      }
+      return null;
+    }
+    // 寻找树的最小键
+    min() {
+      return BinarySearchTree.minNode(this.root);
+    }
+
+    // maxNode
+    static maxNode(node) {
+      if (node) {
+        while (node && node.right !== null) {
+          node = node.right;
+        }
+        return node.key;
+      }
+      return null;
+    }
+    // 寻找树的最大键
+    max() {
+      return BinarySearchTree.maxNode(this.root);
+    }
+
+    static searchNode(node, key) {
+      if (node === null) {
+        return false;
+      }
+      if (key < node.key) {
+        return this.searchNode(node.left, key);
+      } else if (key > node.key) {
+        return this.searchNode(node.right, key);
+      } else {
+        return true;
+      }
+    }
+    // 搜索一个特定的值
+    search(key) {
+      return BinarySearchTree.searchNode(this.root, key);
+    }
+
+    static findMinNode(node) {
+      while (node && node.left !== null) {
+        node = node.left;
+      }
+      return node;
+    }
+    static removeNode(node, element) {}
+    // 移除一个节点
+    remove(element) {
+      this.root = BinarySearchTree.removeNode(this.root, element);
     }
   }
 
@@ -71,8 +157,18 @@ tree.insert(20);
 tree.insert(18);
 tree.insert(25);
 tree.insert(6);
-// console.log(JSON.stringify(tree.root, null, 4));
+tree.insert(27);
+tree.insert(30);
+tree.insert(46);
+tree.insert(70);
+
+console.log(JSON.stringify(tree.root, null, 4));
 function printNode(value) {
-  console.log(value + "-----");
+  console.log(value);
 }
-tree.inOrderTraverse(printNode);
+// tree.inOrderTraverse(printNode);
+// tree.preOrderTraverse(printNode);
+// tree.postOrderTraverse(printNode);
+console.log(tree.min());
+console.log(tree.max());
+console.log(tree.search(1));
