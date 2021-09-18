f3bdca272c7da10183d9c05e88f0eedb7bddb938
赵安东 <zhaoandong4213@sina.com>
2021-09-18 15:32:22 +0800

    imooc

--- new-leetcode/src/algorithm/9-graph.js
+++ new-leetcode/src/algorithm/9-graph.js
@@ -3,6 +3,7 @@
 const Graph = (function () {
   let vertices = [];
   let adjList = new Map();
+  let time = 0;
 
   class Graph {
     addVertex(v) {
@@ -22,6 +23,7 @@ const Graph = (function () {
       }
       return color;
     }
+
     // 图的广度优先搜索算法
     bfs(v, callback) {
       let color = Graph.initializeColor();
@@ -76,7 +78,6 @@ const Graph = (function () {
         predecessors: pred
       };
     }
-
     path(shortestPathA) {
       let fromVertex = vertices[0];
       for (let i = 1; i < vertices.length; i++) {
@@ -99,6 +100,72 @@ const Graph = (function () {
       }
     }
 
+    // 图的深度优先算法，深度优先搜索算法将会从第一个指定的顶点开始遍历图，沿着路径直到这条路径最后一个顶点被访问了，接着原路回退并探索下一条路径。换句话说，它是先深度后广度地访问顶点，
+    // 深度优先搜索算法不需要一个源顶点。在深度优先搜索算法中，若图中顶点v未访问，则访问该顶点v。
+    static dfsVisit(u, color, callback) {
+      color[u] = "grey";
+      if (callback) {
+        callback(u);
+      }
+      let neighbors = adjList.get(u);
+      console.log(neighbors);
+      for (let i = 0; i < neighbors.length; i++) {
+        let w = neighbors[i];
+        if (color[w] === "white") {
+          this.dfsVisit(w, color, callback);
+        }
+      }
+      color[u] = "black";
+    }
+    dfs(callback) {
+      let color = Graph.initializeColor();
+
+      for (let i = 0; i < vertices.length; i++) {
+        if (color[vertices[i]] === "white") {
+          Graph.dfsVisit(vertices[i], color, callback); // 只会被执行一次
+        }
+      }
+    }
+
+    static DFSVisit(u, color, d, f, p) {
+      color[u] = "grey";
+      d[u] = ++time;
+
+      let neighbors = adjList.get(u);
+      for (let i = 0; i < neighbors.length; i++) {
+        let w = neighbors[i];
+        if (color[w] === "white") {
+          p[w] = u;
+          this.DFSVisit(w, color, d, f, p);
+        }
+      }
+      color[u] = "black";
+      f[u] = ++time;
+    }
+    DFS() {
+      let color = Graph.initializeColor(),
+        d = {},
+        f = {},
+        p = {};
+      time = 0;
+      for (let i = 0; i < vertices.length; i++) {
+        f[vertices[i]] = 0;
+        d[vertices[i]] = 0;
+        p[vertices[i]] = null;
+      }
+      for (let i = 0; i < vertices.length; i++) {
+        if (color[vertices[i]] === "white") {
+          Graph.DFSVisit(vertices[i], color, d, f, p); // 代码只运行一次
+        }
+      }
+
+      return {
+        discovery: d,
+        finished: f,
+        predecessors: p
+      };
+    }
+
     getInfo() {
       console.log(vertices);
       console.log(adjList);
@@ -108,7 +175,7 @@ const Graph = (function () {
   return Graph;
 })();
 
-let graph = new Graph();
+/* let graph = new Graph();
 let myVertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 for (let i = 0; i < myVertices.length; i++) {
   graph.addVertex(myVertices[i]);
@@ -131,6 +198,23 @@ function print(v) {
 }
 // graph.bfs("C", print);
 
-let A = graph.BFS("A");
-console.log(A);
-graph.path(A);
+// let A = graph.BFS("A");
+// console.log(A);
+// graph.path(A);
+
+// graph.dfs(print);
+console.log(graph.DFS()); */
+
+let graph = new Graph();
+let myVertices = ["A", "B", "C", "D", "E", "F"];
+for (let i = 0; i < myVertices.length; i++) {
+  graph.addVertex(myVertices[i]);
+}
+graph.addEdge("A", "C");
+graph.addEdge("A", "D");
+graph.addEdge("B", "D");
+graph.addEdge("B", "E");
+graph.addEdge("C", "F");
+graph.addEdge("F", "E");
+var result = graph.DFS();
+console.log(result);
